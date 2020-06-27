a2ae036c14ee304dc57e9ea7d70c24e8d007a5df
Radosław Gorczyca <radoslaw.gorczyca@testerzy.pl>
2020-06-27 14:17:11 +0200

    Ukończone zadanie z kamerą i galerią w projekcie 5

--- app/src/main/java/org/gorczyca/pum/project5/CameraAndGalleryActivity.java
+++ app/src/main/java/org/gorczyca/pum/project5/CameraAndGalleryActivity.java
@@ -1,16 +1,122 @@
 package org.gorczyca.pum.project5;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import org.gorczyca.pum.R;
 
-public class CameraAndGalleryActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class CameraAndGalleryActivity extends AppCompatActivity implements View.OnClickListener {
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private String currentPhotoPath;
+
+    private Button buttonCamera;
+    private Button buttonGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_5_camera_and_gallery);
+        bindIds();
+        bindListeners();
+    }
+
+    private void bindIds() {
+        buttonCamera = findViewById(R.id.button_camera);
+        buttonGallery = findViewById(R.id.button_gallery);
+    }
+
+    private void bindListeners() {
+        buttonCamera.setOnClickListener(this);
+        buttonGallery.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == buttonCamera.getId()) {
+            dispatchTakePictureIntent();
+        } else if (v.getId() == buttonGallery.getId()) {
+            openGallery();
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ignored) {
+
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "org.gorczyca.pum.provider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO) {
+            if (resultCode == Activity.RESULT_OK) {
+                galleryAddPic();
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setAction(android.content.Intent.ACTION_VIEW);
+        intent.setType("image/*");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, getString(R.string.cant_open_gallery), Toast.LENGTH_SHORT).show();
+        }
     }
 }
